9d2af6dfe8e4621263be211c66c78fb5fb5afd39
Tor N. Johnson <tor.nicholas.johnson@gmail.com>
2025-02-13 15:47:28 -0500

    added starter code

---
+++ starter_code/js/data.js
@@ -0,0 +1,102 @@
+const cartInfo = [
+    {
+        pid: 1471,
+        name: "Hay",
+        price: 4.35,
+        quantity: 12
+    },
+    {
+        pid:17,
+        name: "Acme Tractor Spark Plug",
+        price: 5.95,
+        quantity: 4
+    },
+    {
+        pid: 198,
+        name: "Attachment for Acme Tractor: Spreader",
+        price: 1255.00,
+        quantity: 1
+    },
+    {
+        pid: 166,
+        name: "Acme Tractor Seat Cover",
+        price: 15.00,
+        quantity: 1
+    },
+    {
+        pid: 149,
+        name: "Acme Tractor Oil",
+        price: 25.00,
+        quantity: 1
+    },
+    {
+        pid: 1471,
+        name: "Baylor's Best Chicken Feed",
+        price: 10.00,
+        quantity: 18
+    },
+    {
+        pid: 181,
+        name: "Sauron's Fertilizer",
+        price: 15.00,
+        quantity: 4
+    },
+    {
+        pid: 1233,
+        name:"Arrakis Brand Horse De-wormer",
+        price: 12.00,
+        quantity: 2
+    }
+];
+const salesTaxRates = {
+        "Alabama": 4,
+        "Alaska":0,
+        "Arizona":5.6,
+        "Arkansas":6.5,
+        "California":7.25,
+        "Colorado":2.9,
+        "Connecticut":6.35,
+        "Delaware":0,
+        "Florida":6,
+        "Georgia":4,
+        "Hawaii":4,
+        "Idaho":6,
+        "Illinois":6.25,
+        "Indiana":7,
+        "Iowa":6,
+        "Kansas":6.5,
+        "Kentucky":6,
+        "Louisiana":4.45,
+        "Maine":5.5,
+        "Maryland":6,
+        "Massachusetts":6.25,
+        "Michigan":6,
+        "Minnesota":6.875,
+        "Mississippi":7,
+        "Missouri":4.225,
+        "Montana":0,
+        "Nebraska":5.5,
+        "Nevada":6.85,
+        "New Hampshire":0,
+        "New Jersey":6.625,
+        "New Mexico":5.125,
+        "New York":4,
+        "North Carolina":4.75,
+        "North Dakota":5,
+        "Ohio":5.75,
+        "Oklahoma":4.5,
+        "Oregon":0,
+        "Pennsylvania":6,
+        "Rhode Island":7,
+        "South Carolina":6,
+        "South Dakota":4.5,
+        "Tennessee":7,
+        "Texas":6.25,
+        "Utah":4.85,
+        "Vermont":6,
+        "Virginia":5.3,
+        "Washington":6.5,
+        "West Virginia":6,
+        "Wisconsin":5,
+        "Wyoming":4
+    }
